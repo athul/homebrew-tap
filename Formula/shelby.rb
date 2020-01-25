3e4b86e3e50d97762e60ef89fc06c42c108a9d24
athul <athul8720@gmail.com>
2020-01-25 20:09:25 +0530

    Changed the RB file to Darwin package

--- Formula/shelby.rb
+++ Formula/shelby.rb
@@ -2,11 +2,9 @@ class Shelby < Formula
   desc "The Prompt with Wings🦋"
   homepage ""
   url "https://github.com/athul/shelby/releases/download/v0.1.14/shelby_0.1.14_Darwin_x86_64.tar.gz"
-  version "0.1.14"
-  sha256 "bad271b7a63aa0d9e30d80a336ac0d7d087a6124407cbc4c8085d754f2d61c92"
-  
+  sha256 "6c7177ee6037a98d7e4218d10bc1e113d7c7a9239f2853f06bdab097f12358ab"
+
   def install
     bin.install "shelby"
   end
-
 end
