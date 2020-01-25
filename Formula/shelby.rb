d4cef849a5de8be1d7b634c3ee2e8fdee59d6a32
athul <athul8720@gmail.com>
2020-01-25 19:43:57 +0530

    RB Changes

--- Formula/shelby.rb
+++ Formula/shelby.rb
@@ -4,14 +4,14 @@
 class Shelby < Formula
   desc "The Prompt with Wings🦋"
   homepage ""
-  url "https://github.com/athul/shelby/archive/v0.1.14.tar.gz"
+  url "https://github.com/athul/shelby/releases/download/v0.1.14/shelby_0.1.14_Darwin_x86_64.tar.gz"
   version "0.1.14"
   sha256 "bad271b7a63aa0d9e30d80a336ac0d7d087a6124407cbc4c8085d754f2d61c92"
 
   # depends_on "cmake" => :build
 
   def install
-    bin,install "shelby"
+    bin.install "shelby"
   end
 
 end
